7a814004a5484d26c00ed93d66ddc3f1a7d1ba7b
lin yuan <lyuan8733@gmail.com>
2019-03-28 16:26:48 -0600

    finished registration and signin form for pawfect friends socialnetworkClone

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<?php
+require 'config/config.php';
+
+if(isset($_SESSION['username'])){
+    $userLoggedIn = $_SESSION['username'];
+} else{
+    header("Location: register.php"); // stop access to index.php if not logged in 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include("includes/header.php");
+
+// stop access to index.php if not logged in
+session_destroy();
+?>
+    Hello World!
+</body>
+</html>
